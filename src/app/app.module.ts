8cc3c121ca74482787e2e8820a5646af4d208057
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-16 03:35:26 +1000

    remove loginComponent and Account omponent  on ng module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AccountComponent } from './account/account.component';
-
+import { FormsModule } from '@angular/forms'
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    AccountComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
