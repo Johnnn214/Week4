8590857c15ba227b4af9b1f89c7c2200666eb9fa
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-16 03:37:55 +1000

    added variable email and pwd on class logincomponent and signin function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  email:string = "";
+  pwd:string = "";
+  private router = inject(Router);
+
+  ngOnInit() {
+    throw new Error('Method not implemented.');
+  }
+  signin(event:any){
+    this.router.navigate(['/account']);
+  }
 
 }
