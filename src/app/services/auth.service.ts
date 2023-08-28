3d9c3b3b1bda1d330ce27e46655b48f7ac880d0b
Johnnn214 <johnmar.quirona@griffithuni.edu.au>
2023-08-29 01:50:25 +1000

    added auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/user';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+
+  isLoggedin(){
+    if (sessionStorage.getItem('currentUser')){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  login(email:string,password:string){
+    return this.http.post<User>('http://localhost:3000/api/auth', { email: email , password: password});
+  }
+
+  setCurrentuser(newuser:User){
+    sessionStorage.setItem('currentUser',JSON.stringify(newuser));
+
+  }
+  getCurrentuser(){
+    return sessionStorage.getItem('currentUser');
+  }
+
+  logout(event:any){
+    sessionStorage.removeItem('currentUser');
+    this.router.navigateByUrl('/login');
+
+  }
+
+}
